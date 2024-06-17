b949d9b2a1adb63f9d61d149bb76434328b39542
dp-daly <dan.daly@education.gov.uk>
2024-06-17 13:42:35 +0100

    exhibition rendered on gallery page

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from 'react'
+
 function Gallery() {
-    return <h1>hello</h1>
+
+    const [exhibitions, setExhibitions] = useState([])
+
+    useEffect(() => {
+        fetchExhibitions()
+    }, [])
+
+
+    async function fetchExhibitions() {
+        const resp = await fetch('/api/')
+        const data = await resp.json()
+        setExhibitions(data)
+    }
+
+    return (
+        <div>
+            {exhibitions.map((exhibition, index) => (
+                <div key={index}>
+                    <h2>{exhibition.exhibitionTitle}</h2>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default Gallery
