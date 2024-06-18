cae4a43cc981f583e5239582994746cb537c367f
Emma Frith <emma.frith@education.gov.uk>
2024-06-18 14:34:57 +0100

    add delete button

--- src/components/ShowExhibition.jsx
+++ src/components/ShowExhibition.jsx
@@ -21,10 +21,23 @@ function ShowExhibition() {
         fetchExhibitions()
       }, [exhibitionId])
 
+      async function handleClick() {
+        try {
+          const token = localStorage.getItem("token")
+          await axios.delete(`/api/gallery/${exhibitionId}`, {
+            headers: { Authorization: `Bearer ${token}` }
+          })
+          navigate('/')
+        } catch (err) {
+          console.log(err)
+        }
+      }
+
       return <div className="section">
       <div className="container">
         <h1 className="title">{exhibition.exhibitionTitle}</h1>
         {isAdmin() && <Link to={`/gallery/${exhibitionId}/edit`} className="button">Edit</Link>}
+        {isAdmin() && <button className="button is-danger" onClick={handleClick}>Delete {exhibition.exhibitionTitle}</button>}
       </div>
     </div>
 
