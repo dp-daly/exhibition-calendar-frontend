d54dd26865e8ceac3af1d9d3371c3d07f34653ed
dp-daly <dan.daly@education.gov.uk>
2024-06-19 14:36:37 +0100

    fixed bug on planner page refreshing

--- src/components/Planner.jsx
+++ src/components/Planner.jsx
@@ -38,23 +38,23 @@ export default function Planner() {
             await axios.delete(`/api/user/${userId}/${e.target.id}`, {
                 headers: { Authorization: `Bearer ${token}` }
             })
-            navigate(`/user/${userId}`)
         } catch (err) {
             console.log(err)
         }
     }
+
 
     return (
         <div className="page-wrapper">
             <h1 className="title">Your exhibitions</h1>
             <div className="box-wrapper">
                 {user.savedExhibitions.map((exhibition, index) => (
-                    <Link to={`/gallery/${exhibition._id}`} key={index}>
-                        <div>
-                            <div className="img-container">
+                    <div key={index} className="exhibition-card">
+                        <div className="img-container">
+                            <Link to={`/gallery/${exhibition._id}`} className="exhibition-link">
                                 <img className="img-placeholder" src={exhibition.image} alt={exhibition.exhibitionTitle} />
-                                <button id={exhibition._id} className="remove-from-planner-button" onClick={handleRemoveFromPlanner}>x</button>
-                            </div>
+                            </Link>
+                            <button id={exhibition._id} className="remove-from-planner-button" onClick={handleRemoveFromPlanner}>x</button>
                         </div>
                         <div className="card-footer">
                             <div className="exhibition-title">
@@ -64,24 +64,23 @@ export default function Planner() {
                                 {exhibition.museum}
                             </div>
                         </div>
-                    </Link>
+                    </div>
                 ))}
             </div>
             <ToastContainer
-            position="bottom-center"
-            autoClose={2000}
-            hideProgressBar
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="dark"
-            toastStyle={{ backgroundColor: "blue", color: "white" }}
+                position="bottom-center"
+                autoClose={2000}
+                hideProgressBar
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                toastStyle={{ backgroundColor: "blue", color: "white" }}
             />
         </div>
     )
 }
-
 
