a479d3d0425b6315bdb96f3ac49847822e2815c8
Emma Frith <emma.frith@education.gov.uk>
2024-06-19 09:55:32 +0100

    add delete button fuctionality

--- src/components/Planner.jsx
+++ src/components/Planner.jsx
@@ -1,18 +1,37 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { useNavigate, Link } from 'react-router-dom'
+import { getPayload, isAdmin } from '../lib/auth.js'
 
 export default function Planner() {
+
+    const navigate = useNavigate()
+
     const [user, setUser] = useState({ savedExhibitions: [] })
     const { userId } = useParams()
 
     useEffect(() => {
         fetchUser()
-    }, [])
-    
+    }, [user])
+
     async function fetchUser() {
         const resp = await fetch(`/api/user/${userId}`)
         const data = await resp.json()
         setUser(data)
+    }
+
+    async function handleRemoveFromPlanner(e) {
+        try {
+            const userId = getPayload().userId
+            const token = localStorage.getItem("token")
+            await axios.delete(`/api/user/${userId}/${e.target.id}`, {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+            navigate(`/user/${userId}`)
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     return (
@@ -21,11 +40,11 @@ export default function Planner() {
             <ul>
                 {user.savedExhibitions.map((exhibition, index) => (
                     <>
-                    <li key={exhibition._id}>{exhibition.exhibitionTitle}</li>
-                    <div className="img-container">
-                    <li><img className="img-placeholder" src={exhibition.image} alt="" /></li>
-                    <li><button className="remove-from-planner-button">x</button></li>
-                    </div>
+                        <li key={exhibition._id}>{exhibition.exhibitionTitle}</li>
+                        <div className="img-container">
+                            <li><img id={exhibition._id} className="img-placeholder" src={exhibition.image} alt="" /></li>
+                            <li><button id={exhibition._id} className="remove-from-planner-button" onClick={handleRemoveFromPlanner}>x</button></li>
+                        </div>
                     </>
                 ))}
             </ul>
